75f225b5a8beedd1881e7f636ac853546bc515a8
sh_rajin <shrajin7@gmail.com>
2025-01-05 21:21:27 +0600

    env setup

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7j-)0c&zhb32ir4$l-vxm!gl*e3_cs5dc4*&sr7y!3urap5=w7'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
